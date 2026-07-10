b583ed0d681a09aac7c2c40c646117c860d9f48b
ERAC <edwarddelcastillo4@gmail.com>
2026-07-09 21:03:51 -0500

    fix: use full java path in entrypoint; remove obsolete compose version

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 set -e
 mkdir -p /app/data
 chown -R raindrops:raindrops /app/data
-exec su -s /bin/sh raindrops -c "exec java -jar /app/app.jar"
+exec su -s /bin/sh raindrops -c "exec /opt/java/openjdk/bin/java -jar /app/app.jar"
